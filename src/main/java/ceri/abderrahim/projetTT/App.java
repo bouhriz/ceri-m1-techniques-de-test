e1c0313b866d129186631bf714d9e351e0c14c98
Abderrahim Bouhriz <abderrahimbouhriz@gmail.com>
2022-04-16 14:48:28 +0200

    Modification dans TP1

---
+++ src/main/java/ceri/abderrahim/projetTT/App.java
@@ -0,0 +1,13 @@
+package ceri.abderrahim.projetTT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
