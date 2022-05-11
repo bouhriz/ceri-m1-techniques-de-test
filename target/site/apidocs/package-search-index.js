46cc9bb1082f215a24239876ce520162423e7731
Abderrahim Bouhriz <abderrahimbouhriz@gmail.com>
2022-05-11 21:44:41 +0200

    TP5 fini

--- target/site/apidocs/package-search-index.js
+++ target/site/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fr.univavignon.pokedex.api"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"fr.univavignon.pokedex.api"}]
